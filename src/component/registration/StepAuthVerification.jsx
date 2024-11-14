a50e12573cbc0524ddfd060628694da3df8178ee
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-11-14 14:47:49 +0900

    fix: 무직일 경우 회원가입 안되던 버그 수정

--- src/component/registration/StepAuthVerification.jsx
+++ src/component/registration/StepAuthVerification.jsx
@@ -1,207 +1,181 @@
-import React, { useState } from "react";
-import Input from "../basic/Input";
-import Button from "../basic/Button";
-import CloseButton from "../basic/CloseButton";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import Input from '../basic/Input';
+import Button from '../basic/Button';
+import CloseButton from '../basic/CloseButton';
+import axios from 'axios';
 
 function StepAuthVerification({
-  handleInputChange,
-  formData,
-  companyEmail,
-  sendAuthCode,
-  isSend,
-  isActive,
-  minutes,
-  seconds,
-  sendUserInfo,
-  setModalStep,
-  setIsDuplicate,
-  handleCloseWithReset,
+    handleInputChange,
+    formData,
+    companyEmail,
+    sendAuthCode,
+    isSend,
+    isActive,
+    minutes,
+    seconds,
+    sendUserInfo,
+    setModalStep,
+    setIsDuplicate,
+    handleCloseWithReset,
 }) {
-  const [isDuplicate, setIsDuplicateLocal] = useState(false);
-  const [errors, setErrors] = useState({});
+    const [isDuplicate, setIsDuplicateLocal] = useState(false);
+    const [errors, setErrors] = useState({});
+    const handleDuplicateCheck = () => {
+        axios.get(`http://43.203.69.159/api/v1/auth/register/check/${formData.userNickName}`).then((res) => {
+            if (res.data.isExist === false) {
+                alert('사용가능한 아이디입니다');
+                setIsDuplicateLocal(true);
+                setIsDuplicate(true);
+            } else {
+                alert('중복된 아이디입니다');
+                setIsDuplicateLocal(false);
+                setIsDuplicate(false);
+            }
+        });
+    };
 
-  const handleDuplicateCheck = () => {
-    axios
-      .get(
-        `http://43.203.69.159/api/v1/auth/register/check/${formData.userNickName}`
-      )
-      .then((res) => {
-        if (res.data.isExist === false) {
-          alert("사용가능한 아이디입니다");
-          setIsDuplicateLocal(true);
-          setIsDuplicate(true);
-        } else {
-          alert("중복된 아이디입니다");
-          setIsDuplicateLocal(false);
-          setIsDuplicate(false);
+    const handleNextClick = () => {
+        const newErrors = {};
+
+        if (!formData.userNickName) {
+            newErrors.userNickName = '아이디를 입력해주세요';
         }
-      });
-  };
+        if (!formData.password) {
+            newErrors.password = '비밀번호를 입력해주세요';
+        }
+        if (!formData.checkPassword) {
+            newErrors.checkPassword = '비밀번호 확인을 입력해주세요';
+        }
+        if (!formData.email) {
+            newErrors.email = '이메일을 입력해주세요';
+        }
+        if (!formData.authCode) {
+            newErrors.authCode = '인증번호를 입력해주세요';
+        }
 
-  const handleNextClick = () => {
-    const newErrors = {};
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return;
+        }
 
-    if (!formData.userNickName) {
-      newErrors.userNickName = "아이디를 입력해주세요";
-    }
-    if (!formData.password) {
-      newErrors.password = "비밀번호를 입력해주세요";
-    }
-    if (!formData.checkPassword) {
-      newErrors.checkPassword = "비밀번호 확인을 입력해주세요";
-    }
-    if (!formData.email) {
-      newErrors.email = "이메일을 입력해주세요";
-    }
-    if (!formData.authCode) {
-      newErrors.authCode = "인증번호를 입력해주세요";
-    }
+        setErrors({});
+        sendUserInfo();
+    };
 
-    if (Object.keys(newErrors).length > 0) {
-      setErrors(newErrors);
-      return;
-    }
+    const firstErrorField = Object.keys(errors)[0];
 
-    setErrors({});
-    sendUserInfo();
-  };
-
-  const firstErrorField = Object.keys(errors)[0];
-
-  return (
-    <div className="register_modal">
-      <div className="register_modal__header">
-        <button
-          onClick={() => {
-            setModalStep(1);
-          }}
-        >
-          ←
-        </button>
-        <p className="title">가입하기</p>
-        <button>
-          <CloseButton onCloseButton={handleCloseWithReset} />
-        </button>
-      </div>
-      <div className="register_modal__section">
-        <div className="register_modal__idform">
-          <Input
-            size="Large"
-            placeholder={"아이디"}
-            onChange={handleInputChange}
-            value={formData.userNickName}
-            name={"userNickName"}
-            autocomplete={"off"}
-          />
-          <button className="duplication_button" onClick={handleDuplicateCheck}>
-            중복확인
-          </button>
-          {firstErrorField === "userNickName" && (
-            <div
-              className="error-message"
-              style={{ color: "red", marginTop: "5px" }}
-            >
-              *{errors.userNickName}
+    return (
+        <div className="register_modal">
+            <div className="register_modal__header">
+                <button
+                    onClick={() => {
+                        setModalStep(1);
+                    }}
+                >
+                    ←
+                </button>
+                <p className="title">가입하기</p>
+                <button>
+                    <CloseButton onCloseButton={handleCloseWithReset} />
+                </button>
             </div>
-          )}
-        </div>
-        <div className="register_modal__passwordform">
-          <Input
-            size="Large"
-            placeholder={"비밀번호"}
-            onChange={handleInputChange}
-            name={"password"}
-            value={formData.password}
-            type="password"
-            autocomplete={"off"}
-          />
-          {firstErrorField === "password" && (
-            <div
-              className="error-message"
-              style={{ color: "red", marginTop: "5px" }}
-            >
-              *{errors.password}
-            </div>
-          )}
-          <Input
-            size="Large"
-            placeholder={"비밀번호 확인"}
-            onChange={handleInputChange}
-            name={"checkPassword"}
-            value={formData.checkPassword}
-            type="password"
-            autocomplete={"off"}
-          />
-          {firstErrorField === "checkPassword" && (
-            <div
-              className="error-message"
-              style={{ color: "red", marginTop: "5px" }}
-            >
-              *{errors.checkPassword}
-            </div>
-          )}
-        </div>
+            <div className="register_modal__section">
+                <div className="register_modal__idform">
+                    <Input
+                        size="Large"
+                        placeholder={'아이디'}
+                        onChange={handleInputChange}
+                        value={formData.userNickName}
+                        name={'userNickName'}
+                        autocomplete={'off'}
+                    />
+                    <button className="duplication_button" onClick={handleDuplicateCheck}>
+                        중복확인
+                    </button>
+                    {firstErrorField === 'userNickName' && (
+                        <div className="error-message" style={{ color: 'red', marginTop: '5px' }}>
+                            *{errors.userNickName}
+                        </div>
+                    )}
+                </div>
+                <div className="register_modal__passwordform">
+                    <Input
+                        size="Large"
+                        placeholder={'비밀번호'}
+                        onChange={handleInputChange}
+                        name={'password'}
+                        value={formData.password}
+                        type="password"
+                        autocomplete={'off'}
+                    />
+                    {firstErrorField === 'password' && (
+                        <div className="error-message" style={{ color: 'red', marginTop: '5px' }}>
+                            *{errors.password}
+                        </div>
+                    )}
+                    <Input
+                        size="Large"
+                        placeholder={'비밀번호 확인'}
+                        onChange={handleInputChange}
+                        name={'checkPassword'}
+                        value={formData.checkPassword}
+                        type="password"
+                        autocomplete={'off'}
+                    />
+                    {firstErrorField === 'checkPassword' && (
+                        <div className="error-message" style={{ color: 'red', marginTop: '5px' }}>
+                            *{errors.checkPassword}
+                        </div>
+                    )}
+                </div>
 
-        <div className="register_modal__email">
-          <Input
-            placeholder={"이메일"}
-            size={"Large"}
-            name={"email"}
-            value={formData.email}
-            onChange={handleInputChange}
-            autocomplete={"off"}
-          />
-          <span>{companyEmail ? "@" + companyEmail : ""}</span>
-          <button
-            className="auth_button"
-            onClick={sendAuthCode}
-            disabled={isActive}
-          >
-            인증
-          </button>
+                <div className="register_modal__email">
+                    <Input
+                        placeholder={'이메일'}
+                        size={'Large'}
+                        name={'email'}
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        autocomplete={'off'}
+                    />
+                    {companyEmail !== '없음' && <span>{companyEmail ? '@' + companyEmail : ''}</span>}
+                    <button className="auth_button" onClick={sendAuthCode} disabled={isActive}>
+                        인증
+                    </button>
 
-          {firstErrorField === "email" && (
-            <div
-              className="error-message"
-              style={{ color: "red", marginTop: "5px" }}
-            >
-              *{errors.email}
+                    {firstErrorField === 'email' && (
+                        <div className="error-message" style={{ color: 'red', marginTop: '5px' }}>
+                            *{errors.email}
+                        </div>
+                    )}
+                </div>
+                <div className="register_modal__auth">
+                    <Input
+                        placeholder={'인증번호'}
+                        size={'Large'}
+                        name={'authCode'}
+                        value={formData.authCode}
+                        onChange={handleInputChange}
+                        disabled={!isActive}
+                        autocomplete={'off'}
+                    />
+                    {isActive ? (
+                        <span className="auth_time">{`${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`}</span>
+                    ) : (
+                        <span></span>
+                    )}
+                </div>
+                {firstErrorField === 'authCode' && (
+                    <div className="error-message" style={{ color: 'red', marginTop: '5px' }}>
+                        *{errors.authCode}
+                    </div>
+                )}
             </div>
-          )}
+            <div className="register_modal__footer">
+                <Button text={'다음'} size={'Large'} onClick={handleNextClick} />
+            </div>
         </div>
-        <div className="register_modal__auth">
-          <Input
-            placeholder={"인증번호"}
-            size={"Large"}
-            name={"authCode"}
-            value={formData.authCode}
-            onChange={handleInputChange}
-            disabled={!isActive}
-            autocomplete={"off"}
-          />
-          {isActive ? (
-            <span className="auth_time">{`${minutes}:${
-              seconds < 10 ? `0${seconds}` : seconds
-            }`}</span>
-          ) : (
-            <span></span>
-          )}
-        </div>
-        {firstErrorField === "authCode" && (
-          <div
-            className="error-message"
-            style={{ color: "red", marginTop: "5px" }}
-          >
-            *{errors.authCode}
-          </div>
-        )}
-      </div>
-      <div className="register_modal__footer">
-        <Button text={"다음"} size={"Large"} onClick={handleNextClick} />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default StepAuthVerification;
--- src/component/registration/StepCompanySelection.jsx
+++ src/component/registration/StepCompanySelection.jsx
@@ -1,89 +1,87 @@
-import React, { useState } from "react";
-import Input from "../basic/Input";
-import Button from "../basic/Button";
-import CloseButton from "../basic/CloseButton";
+import React, { useState } from 'react';
+import Input from '../basic/Input';
+import Button from '../basic/Button';
+import CloseButton from '../basic/CloseButton';
 
 function StepCompanySelection({
-  handleCompanyName,
-  companyName,
-  companys,
-  getCompanyEmail,
-  setModalStep,
-  handleCloseWithReset,
+    handleCompanyName,
+    companyName,
+    companys,
+    getCompanyEmail,
+    setModalStep,
+    handleCloseWithReset,
 }) {
-  const [selectedCompanyName, setSelectedCompanyName] = useState("");
+    const [selectedCompanyName, setSelectedCompanyName] = useState('');
 
-  const handleCompanyClick = (companyName) => {
-    setSelectedCompanyName(companyName);
-    getCompanyEmail(companyName);
-  };
+    const handleCompanyClick = (companyName) => {
+        setSelectedCompanyName(companyName);
+        getCompanyEmail(companyName);
+    };
 
-  return (
-    <div className="register_modal">
-      <div className="register_modal__header">
-        <button>
-          <CloseButton onCloseButton={handleCloseWithReset} />
-        </button>
-        <p className="title">가입하기</p>
-      </div>
+    return (
+        <div className="register_modal">
+            <div className="register_modal__header">
+                <button>
+                    <CloseButton onCloseButton={handleCloseWithReset} />
+                </button>
+                <p className="title">가입하기</p>
+            </div>
 
-      <div className="register_modal__section">
-        <div className="register_section__form">
-          <Input
-            size="Large"
-            name="companyName"
-            onChange={handleCompanyName}
-            autocomplete={"off"}
-            placeholder={"회사 검색(무직인 경우 '없음' 입력)"}
-          />
-          <div className="register_section__result">
-            {companys.length > 0 ? (
-              <ul>
-                {companys.map((company, index) => (
-                  <li key={index}>
-                    {company.name}
-                    <button
-                      onClick={() => handleCompanyClick(company.name)}
-                      style={{
-                        backgroundColor:
-                          selectedCompanyName === company.name
-                            ? "#60aeb3"
-                            : "#71c9ce",
-                        color:
-                          selectedCompanyName === company.name
-                            ? "black"
-                            : "white",
-                      }}
-                    >
-                      선택
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            ) : companyName.length > 0 ? (
-              <p>검색 결과가 없습니다.</p>
-            ) : (
-              <></>
-            )}
-          </div>
+            <div className="register_modal__section">
+                <div className="register_section__form">
+                    <Input
+                        size="Large"
+                        name="companyName"
+                        onChange={handleCompanyName}
+                        autocomplete={'off'}
+                        placeholder={"회사 검색(무직인 경우 '없음' 입력)"}
+                    />
+                    <div className="register_section__result">
+                        {companys.length > 0 ? (
+                            <ul>
+                                {companys.map((company, index) => (
+                                    <li key={index}>
+                                        {company.name}
+                                        <button
+                                            onClick={() => handleCompanyClick(company.name)}
+                                            style={{
+                                                backgroundColor:
+                                                    selectedCompanyName === company.name ? '#60aeb3' : '#71c9ce',
+                                                color: selectedCompanyName === company.name ? 'black' : 'white',
+                                            }}
+                                        >
+                                            선택
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : companyName.length > 0 ? (
+                            <p>검색 결과가 없습니다.</p>
+                        ) : (
+                            <></>
+                        )}
+                    </div>
+                </div>
+            </div>
+
+            <div className="register_modal__footer">
+                <Button
+                    text={'다음'}
+                    size={'Large'}
+                    onClick={() => {
+                        if (selectedCompanyName) {
+                            setModalStep(1);
+                        } else if (selectedCompanyName === '' && companyName === '없음') {
+                            setSelectedCompanyName('없음');
+                            getCompanyEmail('없음');
+                        } else {
+                            alert(`회사를 선택해 주세요`);
+                        }
+                    }}
+                />
+            </div>
         </div>
-      </div>
-
-      <div className="register_modal__footer">
-        <Button
-          text={"다음"}
-          size={"Large"}
-          onClick={() => {
-            if (selectedCompanyName) {
-              setModalStep(1);
-            } else {
-              alert("회사를 선택해 주세요");
-            }
-          }}
-        />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default StepCompanySelection;
